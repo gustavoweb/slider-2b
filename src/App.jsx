f83663789ca90528778a14b095a6c5f9e9c6cb2c
gustavoweb <contato@gustavoweb.com>
2025-09-01 10:40:33 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
-import TenisNike from './assets/tenis-nike.jpg'
+// import TenisNike from './assets/tenis-nike.jpg'
+import Carmy from './assets/cap-army.jpg'
+import Cjoa from './assets/cap-joaninha.jpg'
+import Curs from './assets/cap-ursinho.avif'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,36 +17,36 @@ function App() {
           {/* Produto 1 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={TenisNike} alt="" className="img-prod"/>
-              <h2>Tenis Nike</h2>
-              <p className="desc">Um lindo tenis da nike</p>
-              <p className="preco">R$ 699,99</p>
-              <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
-              <div className="off">- 15%</div>
+              <img src={Cjoa} alt="" className="img-prod" />
+              <h2 className="titulo-card">Capacete Joaninha</h2>
+              <p className="desc">Proteção ao nível kevlar acoplada ao Estilo Joaninha (Rodolia cardinalis)</p>
+              <p className="preco">R$ 299,99</p>
+              <div className="avaliacao"> <span>★ ★ ★ ★ ★</span> (72)</div>
+              <div className="off">- 10%</div>
             </div>
           </a>
 
           {/* Produto 2 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={TenisNike} alt="" className="img-prod"/>
-              <h2>Tenis Nike</h2>
-              <p className="desc">Um lindo tenis da nike</p>
-              <p className="preco">R$ 699,99</p>
-              <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
-              <div className="off">- 15%</div>
+              <img src={Carmy} alt="" className="img-prod" />
+              <h2 className="titulo-card">Capacete PUBG</h2>
+              <p className="desc">Seu galinaceo protegido sem perder o estilo militar dos games online de battle royale.</p>
+              <p className="preco">R$ 399,99</p>
+              <div className="avaliacao"> <span>★ ★ ★ ★ ☆</span> (15)</div>
+              <div className="off">- 5%</div>
             </div>
           </a>
 
           {/* Produto 3 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={TenisNike} alt="" className="img-prod"/>
-              <h2>Tenis Nike</h2>
-              <p className="desc">Um lindo tenis da nike</p>
-              <p className="preco">R$ 699,99</p>
-              <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
-              <div className="off">- 15%</div>
+              <img src={Curs} alt="" className="img-prod" />
+              <h2 className="titulo-card">Capacete Urso Ted</h2>
+              <p className="desc">Une proteção, segurança e clarto um dos personagens mais carismáticos do cinema.</p>
+              <p className="preco">R$ 299,99</p>
+              <div className="avaliacao"> <span>★ ★ ★ ★ ★</span> (302)</div>
+              <div className="off">- 10%</div>
             </div>
           </a>
 
