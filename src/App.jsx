a322e5d3111b66a6175381a17f7e13e670bca048
gustavoweb <contato@gustavoweb.com>
2025-09-15 07:30:40 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-// import TenisNike from './assets/tenis-nike.jpg'
 import Carmy from './assets/cap-army.jpg'
 import Cjoa from './assets/cap-joaninha.jpg'
 import Curs from './assets/cap-ursinho.avif'
+import Slider1 from './assets/slider1.jpg'
+import Slider2 from './assets/slider2.jpg'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper/modules'
+import 'swiper/css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,13 +15,38 @@ function App() {
   return (
     <>
       <main>
-        <header></header>
+        <header>
+          <Swiper
+            modules={[Autoplay]}
+            spaceBetween={0}
+            slidesPerView={1}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src={Slider1} alt="Slide 1" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider2} alt="Slide 2" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider1} alt="Slide 3" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider2} alt="Slide 3" />
+            </SwiperSlide>
+          </Swiper>
+        </header>
         <section className="produtos">
 
           {/* Produto 1 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={Cjoa} alt="" className="img-prod" />
+              <img src={Cjoa} alt="" className="img-card" />
               <h2 className="titulo-card">Capacete Joaninha</h2>
               <p className="desc">Proteção ao nível kevlar acoplada ao Estilo Joaninha (Rodolia cardinalis)</p>
               <p className="preco">R$ 299,99</p>
@@ -29,7 +58,7 @@ function App() {
           {/* Produto 2 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={Carmy} alt="" className="img-prod" />
+              <img src={Carmy} alt="" className="img-card" />
               <h2 className="titulo-card">Capacete PUBG</h2>
               <p className="desc">Seu galinaceo protegido sem perder o estilo militar dos games online de battle royale.</p>
               <p className="preco">R$ 399,99</p>
@@ -41,7 +70,7 @@ function App() {
           {/* Produto 3 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={Curs} alt="" className="img-prod" />
+              <img src={Curs} alt="" className="img-card" />
               <h2 className="titulo-card">Capacete Urso Ted</h2>
               <p className="desc">Une proteção, segurança e clarto um dos personagens mais carismáticos do cinema.</p>
               <p className="preco">R$ 299,99</p>
@@ -51,8 +80,32 @@ function App() {
           </a>
 
         </section>
-        <section className="destaque"></section>
-        <footer></footer>
+        <section className="destaque">
+
+          <iframe src="https://www.youtube.com/embed/p7cDN1ofIw0?autoplay=1&mute=1&controls=0&loop=1&playlist=p7cDN1ofIw0&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
+
+        </section>
+        
+        <footer>
+
+          <img src="" alt="" className="logo-footer" />
+
+          <div className="social-icons">
+            <div className="icon">
+              <img src="" alt="" id="instagram" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="discord" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="x" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="youtube" />
+            </div>
+          </div>
+
+        </footer>
       </main>
     </>
   )
